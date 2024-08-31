4ed8604a4c75e0fbec019814de08230b2290433d
ProfessorQu <quint@dasookwat.com>
2024-08-31 18:31:42 +0200

    Added navigation bar. Added list page.

--- create_website.py
+++ create_website.py
@@ -15,8 +15,14 @@ def create_index(albums):
                 pass
 
         with tag("body"):
-            with tag("h1"):
-                text("Album Ratings")
+            with tag("ul", klass="navbar"):
+                with tag("li"):
+                    with tag("a", href="index.html"):
+                        text("Home")
+
+                with tag("li"):
+                    with tag("a", href="list.html"):
+                        text("List")
 
             with tag("div", klass="album-list"):
                 for album in albums:
@@ -32,6 +38,74 @@ def create_index(albums):
         index_file.write(index_content)
 
 
+def create_list(albums):
+    doc, tag, text = Doc().tagtext()
+
+    doc.asis("<!DOCTYPE html>")
+    with tag("html"):
+        with tag("head"):
+            doc.line("title", "List")
+            doc.stag("link", rel="icon", href="assets/icon.png")
+            doc.stag("link", rel="stylesheet", href="static/stylesheet.css")
+
+        with tag("body"):
+            with tag("ul", klass="navbar"):
+                with tag("li"):
+                    with tag("a", href="index.html"):
+                        text("Home")
+
+                with tag("li"):
+                    with tag("a", href="list.html"):
+                        text("List")
+
+            with tag("table"):
+                with tag("tr"):
+                    with tag("th"):
+                        text("Image")
+                    with tag("th"):
+                        text("Name")
+                    with tag("th"):
+                        text("Artists")
+                    with tag("th"):
+                        text("Recommended by")
+                    with tag("th"):
+                        text("Rating")
+
+                albums.sort(key=lambda x: x['rating'], reverse=True)
+
+                for album in albums:
+                    with tag("tr", klass=album['recommended by'].lower()):
+                        with tag("td"):
+                            with tag("a",
+                                     href=f"albums/{album['name']}.html"):
+                                doc.stag("img", src=album['image'],
+                                         style="width: 5em;")
+
+                        with tag("td"):
+                            with tag("a", href=album['link']):
+                                text(album['name'])
+
+                        with tag("td"):
+                            for i, artist in enumerate(album['artists']):
+                                with tag("a", href=artist['link'],
+                                         target="_blank"):
+                                    text(artist['name'])
+
+                                if i < len(album['artists']) - 1:
+                                    text(" & ")
+
+                        with tag("td", klass="text"):
+                            text(album['recommended by'])
+
+                        with tag("td"):
+                            text(f"{album['rating']} / 10")
+
+    index_content = indent(doc.getvalue())
+
+    with open("list.html", "w") as index_file:
+        index_file.write(index_content)
+
+
 def create_album(album):
     doc, tag, text = Doc().tagtext()
 
@@ -43,6 +117,15 @@ def create_album(album):
             doc.stag("link", rel="stylesheet", href="../static/stylesheet.css")
 
         with tag("body"):
+            with tag("ul", klass="navbar"):
+                with tag("li"):
+                    with tag("a", href="../index.html"):
+                        text("Home")
+
+                with tag("li"):
+                    with tag("a", href="../list.html"):
+                        text("List")
+
             with tag("h1"):
                 with tag("a", href=album['link'], target="_blank"):
                     text(album['name'])
@@ -61,34 +144,37 @@ def create_album(album):
                     doc.stag("img", src=album['image'])
 
                 with tag("div", klass="text"):
-                    with tag("b"):
-                        text("Recommended by ")
-                    text(album['recommended by'])
-
-                    doc.stag("br")
-                    with tag("b"):
-                        text("Rating ")
-                    text(f"{album['rating']} / 10")
-
-                    doc.stag("br")
-                    with tag("b"):
-                        text("Best Songs ")
-                    with tag("ul"):
-                        for song in album['best songs']:
-                            with tag("li"):
-                                with tag("a", href=song['link'],
-                                         target="_blank"):
-                                    text(song['name'])
-
-                    doc.stag("br")
-                    with tag("b"):
-                        text("Comment ")
-                    text(album['comment'])
-
+                    _create_content(tag, text, album, doc)
     album_content = indent(doc.getvalue())
 
     with open(f"albums/{album['name']}.html", "w") as album_file:
         album_file.write(album_content)
+
+
+def _create_content(tag, text, album, doc):
+    with tag("b"):
+        text("Recommended by ")
+    text(album['recommended by'])
+
+    doc.stag("br")
+    with tag("b"):
+        text("Rating ")
+    text(f"{album['rating']} / 10")
+
+    doc.stag("br")
+    with tag("b"):
+        text("Best Songs ")
+    with tag("ul"):
+        for song in album['best songs']:
+            with tag("li"):
+                with tag("a", href=song['link'],
+                         target="_blank"):
+                    text(song['name'])
+
+    doc.stag("br")
+    with tag("b"):
+        text("Comment ")
+    text(album['comment'])
 
 
 def create_albums(albums):
@@ -102,4 +188,5 @@ if __name__ == "__main__":
         albums = json.load(data_file)
 
     create_index(albums)
+    create_list(albums)
     create_albums(albums)
